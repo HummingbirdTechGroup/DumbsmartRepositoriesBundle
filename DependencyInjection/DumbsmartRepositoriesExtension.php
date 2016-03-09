950db88eec9e2202b14bd69727b7faa65cd91ae2
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2016-03-09 17:08:51 +0000

    Fix namespace typo

--- DependencyInjection/DumbsmartRepositoriesExtension.php
+++ DependencyInjection/DumbsmartRepositoriesExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace carlosV2\DumbsmartRepositoriesBundle\DependecyInjection;
+namespace carlosV2\DumbsmartRepositoriesBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
